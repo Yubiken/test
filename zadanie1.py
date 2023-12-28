4e90fa053499ede5e9f4881c80422e5dd7fc5618
Yubiken <54063328+Yubiken@users.noreply.github.com>
2023-12-28 21:47:53 +0100

    Zadanie rekrutacyjne I

---
+++ zadanie1.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+
+
+def login():
+    """Celem funkcji jest zalogowanie się do strony http://demo.testarena.pl i potwierdzenie prawidłowego zalogowania"""
+
+    # inicjalizacja sterownika przeglądarki
+    driver = webdriver.Firefox()
+
+    # otwarcie strony internetowej testarena.pl/zaloguj
+    driver.get("http://demo.testarena.pl/zaloguj")
+
+    # Pobranie loginu i hasła
+    with open('C:\\Users\\y089603\\AppData\\Local\\Programs\\Python\\Python310\\PRACAA\\venv\\konfig.json') as config_file:
+        config = json.load(config_file)
+
+    email = config['email']
+    password = config['password']
+
+    #Logowanie
+    driver.find_element(By.ID, "email").send_keys(email)
+    driver.find_element(By.ID,"password").send_keys(password)
+
+    login = driver.find_element(By.ID, "login")
+    login.click()
+
+    logo_element = driver.find_element(By.ID,"header_logo")
+
+    #Sprawdzenie poprawności logowania do strony
+    if logo_element.is_displayed():
+        print("Logowanie prawidłowe")
+    else:
+        print("Logowanie nieprawidłowe")
+    
+    driver.quit()
+
+login()
